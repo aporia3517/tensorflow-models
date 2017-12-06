24aa72f54a42430a0ea5750f5484bc454b3358b6
Stefan Webb <stefan.webb@wadh.ox.ac.uk>
2017-12-05 21:52:23 -0800

    More bug fixes

--- tensorflow_models/evaluation/iwae.py
+++ tensorflow_models/evaluation/iwae.py
@@ -55,8 +55,8 @@ class Evaluator(BaseEvaluator):
 			this_iwae = self.sess.run(test_iwae_op, feed_dict={x_test: self._tensors})
 	
 		print('epoch {:.3f}, test loss = {:.2f}/{:.2f}/{:.2f}, {:.1f} sec'.format(self.epoch(), test_elbo, test_iwae, this_iwae, iwae_timer.interval))
-		self.ais_results['iwae'].append(this_iwae)
-		self.ais_results['iwae_epoch'].append(self.epoch())
+		self.iwae_results['iwae'].append(this_iwae)
+		self.iwae_results['iwae_epoch'].append(self.epoch())
 
 	def before_step_hook(self):
 		pass
